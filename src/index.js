4d67c0524e0a00d334e9c3c73816d8cdbf0fdb0a
dimasiberia <dima.siberia@icloud.com>
2023-07-31 17:35:22 +0700

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+let size = String(n).replace("-",'').length;
+let answer = '';
+for(let i = size-1; i<size; i--){
+answer += n[i];
+}
+return Number(answer);
 }
